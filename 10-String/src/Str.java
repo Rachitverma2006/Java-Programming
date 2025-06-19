b7be87512f405f28e341db324cc09249646ede62
vxrac <rachitv00@gmail.com>
2025-06-20 00:00:11 +0530

    String

---
+++ 10-String/src/Str.java
@@ -0,0 +1,13 @@
+public class Str {
+    public static void main(String[] args) {
+//        Creating a String
+
+        String str = new String("Hello World");
+        System.out.println(str);
+        String str2 = "World";
+        String str3 = new String();
+        str3 = "Hello";
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
